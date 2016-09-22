495243df0e21a96f3d441c44c67d9ab78cbd2239
Rafael Passos <rafa_auyer@icloud.com>
2016-09-22 10:29:19 -0300

    Update urls.py

--- ifbAMPdb/ifbAMPdatabase/urls.py
+++ ifbAMPdb/ifbAMPdatabase/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='Home'),
     url(r'^search/$', views.search, name='Search'),
     url(r'^amp/(?P<pk>)/$', views.ampInfo, name='AMP Info'), 
