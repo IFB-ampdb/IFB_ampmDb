cf328cfca86b3154f7f3f5195302800712136b47
rafael <rafa_auyer@icloud.com>
2016-09-22 11:37:25 -0300

    fixed models.py

--- ifbAMPdb/ifbAMPdatabase/models.py
+++ ifbAMPdb/ifbAMPdatabase/models.py
@@ -17,6 +17,7 @@ class peptide(models.Model):
     charge_amt_atm = models.FloatField()
     m_dipol_amt_atm = models.FloatField()
     sequence = models.TextField()
+
     def __str__(self):
-        return self.id
+        return self.pdb_id
 
