06e0ede8383e9873bdb8c00dbaa8cdbd3ae712ba
Rafael Passos <rafa_auyer@icloud.com>
2016-09-26 21:28:25 -0300

    Update views.py

--- ifbAMPdb/ifbAMPdatabase/views.py
+++ ifbAMPdb/ifbAMPdatabase/views.py
@@ -3,13 +3,13 @@ from .models import peptide
 # Create your views here.
 
 def home(request):
-    amp = peptide.objects.all()
+    peptide = peptide.objects.all()
     return render(request, 'home.html', {'peptide': peptide})
 
 def search(request):
-    amp = peptide.objects.all()
+    peptide = peptide.objects.all()
     return render(request, 'search.html', {'peptide': peptide})
 
 def ampInfo(request,pk):
-    amp = get_object_or_404(peptide, pk=pk)
+    peptide = get_object_or_404(peptide, pk=pk)
     return render(request,'ampinfo.html', {'peptide' : peptide})
