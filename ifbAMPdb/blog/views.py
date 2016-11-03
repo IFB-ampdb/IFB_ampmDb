168bb55abeae4f33f5418a4030df2263c801d3d7
Rafael Auyer <rafa_auyer@icloud.com>
2016-11-03 17:19:10 -0200

    init blog/views.py

--- ifbAMPdb/blog/views.py
+++ ifbAMPdb/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def homeBlog():
+    pst = Post.objects.order_by(published_date)[:3]
+    return render({'posts': pst})
