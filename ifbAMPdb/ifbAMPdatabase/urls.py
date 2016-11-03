8fbc2b1069ad0060de8b2903d1cdd414673f55a6
Rafael Auyer <rafa_auyer@icloud.com>
2016-11-03 17:47:39 -0200

    update html

--- ifbAMPdb/ifbAMPdatabase/urls.py
+++ ifbAMPdb/ifbAMPdatabase/urls.py
@@ -3,9 +3,10 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    url(r'', include(blog.urls)),
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='Home'),
     url(r'^search/$', views.search, name='Search'),
     url(r'^adv_search/$', views.adv_search, name='Advanced Search'),
-    url(r'^amp/(?P<pk>)/$', views.resoult, name='AMP Info'), 
+    url(r'^amp/(?P<pk>)/$', views.resoult, name='AMP Info'),
 ]
