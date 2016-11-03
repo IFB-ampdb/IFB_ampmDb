bbc56d8239dea6d93c273812eb277c98991662e2
Rafael Auyer <rafa_auyer@icloud.com>
2016-11-03 17:18:11 -0200

    update views and home page

--- ifbAMPdb/ifbAMPdatabase/views.py
+++ ifbAMPdb/ifbAMPdatabase/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import peptide
+from blog.models import homeBlog
+
 # Create your views here.
 
 def home(request):
+    reqHomeData = homeData()
+    return homeData() + homeBlog()
+
+def homeData(request):
     pep = peptide.objects.order_by()[:2]
     return render(request, 'home.html', {'peptide': pep})
 
@@ -16,4 +22,3 @@ def search(request):
 def resoult(request,pk):
     pep = get_object_or_404(peptide, pk=pk)
     return render(request,'ampinfo.html', {'peptide' : pep})
-
