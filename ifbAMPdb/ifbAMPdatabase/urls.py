65f79bfda654723a703ec8985d03f95bd368d00e
Rafael Auyer <rafa_auyer@icloud.com>
2016-11-08 09:33:21 -0200

    added detail url

--- ifbAMPdb/ifbAMPdatabase/urls.py
+++ ifbAMPdb/ifbAMPdatabase/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
 	url(r'^$', views.home, name='Home'),
 	url(r'^search/$', views.search, name='Search'),
 	url(r'^adv_search/$', views.adv_search, name='Advanced Search'),
-	url(r'^amp/(?P<pk>)/$', views.resoult, name='AMP Info'),
+	url(r'^peptide/(?P<pk>)/$', views.detail, name='AMP Detailes'),
 	url(r'^ampBasicSeach/$', views.ampBasicSearch, name='ampBasicSeach'),
 ]
