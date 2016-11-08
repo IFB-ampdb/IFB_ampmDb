806d0c6fac8e136d66cd9c524ccc8c5f6166904f
rafael <rafa_auyer@icloud.com>
2016-11-08 10:54:02 -0200

    regex in detail view updated

--- ifbAMPdb/ifbAMPdatabase/urls.py
+++ ifbAMPdb/ifbAMPdatabase/urls.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-	url(r'', include('blog.urls')),
-	url(r'^admin/', admin.site.urls),
 	url(r'^$', views.home, name='Home'),
 	url(r'^search/$', views.search, name='Search'),
 	url(r'^adv_search/$', views.adv_search, name='Advanced Search'),
-	url(r'^peptide/(?P<pk>)/$', views.detail, name='AMP Detailes'),
+	url(r'^peptide/(?P<pk>[A-Za-z1-9.:]+)/$', views.detail, name='AMP Detailes'),
 	url(r'^ampBasicSeach/$', views.ampBasicSearch, name='ampBasicSeach'),
+	url(r'', include('blog.urls')),
+	url(r'^admin/', admin.site.urls),
 ]
