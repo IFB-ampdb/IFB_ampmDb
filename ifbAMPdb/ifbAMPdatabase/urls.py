35bbdad0bad8a0aca65e5c1d36d41f72d44963a8
rafael <rafa_auyer@icloud.com>
2016-11-09 19:16:43 -0200

    modifed advanced url to /search/advanced/

--- ifbAMPdb/ifbAMPdatabase/urls.py
+++ ifbAMPdb/ifbAMPdatabase/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.home, name='Home'),
 	url(r'^search/$', views.search, name='Search'),
-	url(r'^advSearch/$', views.advSearch, name='Advanced Search'),
+	url(r'^search/advanced/$', views.advSearch, name='Advanced Search'),
 	url(r'^peptide/(?P<pk>[A-Za-z1-9.:]+)/$', views.detail, name='AMP Detailes'),
 	url(r'^ampBasicSeach/$', views.ampBasicSearch, name='ampBasicSeach'),
 	url(r'', include('blog.urls')),
