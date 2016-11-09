f6b356585bf6dd147e13128ee3e86dc02a5f8e1d
rafael <rafa_auyer@icloud.com>
2016-11-09 19:01:59 -0200

    advanced search url

--- ifbAMPdb/ifbAMPdatabase/urls.py
+++ ifbAMPdb/ifbAMPdatabase/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.home, name='Home'),
 	url(r'^search/$', views.search, name='Search'),
-	url(r'^adv_search/$', views.adv_search, name='Advanced Search'),
+	url(r'^advSearch/$', views.advSearch, name='Advanced Search'),
 	url(r'^peptide/(?P<pk>[A-Za-z1-9.:]+)/$', views.detail, name='AMP Detailes'),
 	url(r'^ampBasicSeach/$', views.ampBasicSearch, name='ampBasicSeach'),
 	url(r'', include('blog.urls')),
