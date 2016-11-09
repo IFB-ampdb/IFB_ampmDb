425c4829bd32c692aaa837d63489defb4dea2792
rafael <rafa_auyer@icloud.com>
2016-11-09 21:14:40 -0200

    update search urls

--- ifbAMPdb/ifbAMPdatabase/urls.py
+++ ifbAMPdb/ifbAMPdatabase/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.home, name='Home'),
-	url(r'^search/$', views.search, name='Search'),
-	url(r'^search/advanced/$', views.advSearch, name='Advanced Search'),
-	url(r'^peptide/(?P<pk>[A-Za-z1-9.:]+)/$', views.detail, name='AMP Detailes'),
 	url(r'^ampBasicSeach/$', views.ampBasicSearch, name='ampBasicSeach'),
+	url(r'^peptide/(?P<pk>[A-Za-z1-9.:]+)/$', views.detail, name='AMP Details'),
+	url(r'^search/advanced/$', views.advSearch, name='Advanced Search'),
+	url(r'^search/advanced/resoults$', views.advSearchResoults, name='Advanced Resoults'),
 	url(r'', include('blog.urls')),
 	url(r'^admin/', admin.site.urls),
 ]
