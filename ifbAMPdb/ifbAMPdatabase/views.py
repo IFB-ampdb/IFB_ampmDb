1bf469925a899434661eb850cb397864ba637951
rafael <rafa_auyer@icloud.com>
2016-11-09 21:17:11 -0200

    advanced search test update

--- ifbAMPdb/ifbAMPdatabase/views.py
+++ ifbAMPdb/ifbAMPdatabase/views.py
@@ -58,7 +58,27 @@ def ampBasicSearch(request):
 		pep = peptide.objects.filter(organism = org)
 		for qr in pep:
 			pepList.append(qr)
-		return	render(request, 'resoult.html',{'peptides':pepList, 'searchTerms':searchTerms})
+		return	render(request, 'advResoults.html',{'peptides':pepList, 'searchTerms':searchTerms})
 
 def advSearch(request):
-	return render(request, 'adv_search.html')
+	return render(request, 'advSearch.html')
+
+def advSearchResoults(request):
+	pdb_id = request.GET['pdb_id']
+	hfobic_area = request.GET['hfobic_area']
+	hfobic_avg = request.GET['hfobic_avg']
+	hairpin = request.GET['hairpin']
+	beta_sheet = request.GET['beta_sheet']
+	alpha_helix = request.GET['alpha_helix']
+	alpha_helix_beta_sheet = request.GET['alpha_helix_beta_sheet']
+	alpha_helix_beta_sheet_hairpin = request.GET['alpha_helix_beta_sheet_hairpin']
+	charge = request.GET['charge']
+	m_dipol = request.GET['m_dipol']
+	charge_amt_atm = request.GET['charge_amt_atm']
+	m_dipol_amt_atm = request.GET['m_dipol_amt_atm']
+	sequence = request.GET['sequence']
+	organism = request.GET['organism'] 
+	
+	searchTerms= [{'PDB iD': pdb_id}, {'Hidrofobic Area':hfobic_area}, {'Average Hidrofobicity':hfobic_avg}, {'Hairpin':hairpin}, {'Beta Sheet':beta_sheet}, {'Aplha Helix':alpha_helix}, {'Alpha Helix Beta Sheet':alpha_helix_beta_sheet} , {'Alpha Helix Beta Sheet Hairpin':alpha_helix_beta_sheet_hairpin}, {'Charge':charge}, {'Dipolar Momentum':m_dipol}, {'Charge / Amout of Atoms':charge_amt_atm}, {'Dipolar Momentum / amout of atoms':m_dipol_amt_atm}, {'Sequence':sequence}, {'Organism':organism}]
+	
+	return render(request, 'advResoults.html', {'searchTerms':searchTerms})
