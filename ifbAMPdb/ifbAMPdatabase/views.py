8d5679908c358fc8824799217c1c0b6d9b4312ab
Rafael Auyer <rafa_auyer@icloud.com>
2016-11-10 17:25:23 -0200

    Exception caching added

--- ifbAMPdb/ifbAMPdatabase/views.py
+++ ifbAMPdb/ifbAMPdatabase/views.py
@@ -7,7 +7,6 @@ from blog.views import homeBlog
 # Create your views here.
 
 def home(request):
-	#reqHomeData = homeData()
 	reqHomeBlog = homeBlog()
 	pep = peptide.objects.order_by()[:2]
 	data = {
@@ -29,56 +28,160 @@ def detail(request,pk):
 	return render(request,'detail.html', {'peptide' : pep})
 
 
+# Collects data from the Basic search in the Home Page and displays in the Resoult page.
 def ampBasicSearch(request):
 	pdbId = None
 	org = None
 	searchTerms = []
 	pepList = []
-	if 'pdbId' in request.GET:
-		pdbId = request.GET['pdbId']
-		if not pdbId is '':
-			searchTerms.append(pdbId)
-	if 'org' in request.GET:
-		org = request.GET['org']
-		if not org is '':
-			searchTerms.append(org)
+	#Search items
+	try:
+		if 'pdbId' in request.GET:
+			pdbId = request.GET['pdbId']
+			if not pdbId is '':
+				searchTerms.append(pdbId)
+		if 'org' in request.GET:
+			org = request.GET['org']
+			if not org is '':
+				searchTerms.append(org)
+	except Exception as e:
+		raise #treatment
+
 	#search Conditions
 	if not pdbId is	''	and	not	org	is	'':
-		pep = peptide.objects.filter(pdb_id = pdbId).filter(organism = org)
+		try:
+			pep = peptide.objects.filter(pdb_id = pdbId).filter(organism = org)
+		except Exception as e:
+			raise #Expetion failed to filter item
+
 		for qr in pep:
 			pepList.append(qr)
 		return	render(request, 'resoult.html',{'peptides':pepList, 'searchTerms':searchTerms})
 
 	elif not pdbId is '':
-		pep = peptide.objects.filter(pdb_id = pdbId)
+		try:
+			pep = peptide.objects.filter(pdb_id = pdbId)
+		except Exception as e:
+			raise #failed to filter item
+
 		for qr in pep:
 			pepList.append(qr)
 		return	render(request, 'resoult.html',{'peptides':pepList, 'searchTerms':searchTerms})
 	else:
-		pep = peptide.objects.filter(organism = org)
+		try:
+			pep = peptide.objects.filter(organism = org)
+		except Exception as e:
+			raise #failed to filter item
+
 		for qr in pep:
 			pepList.append(qr)
 		return	render(request, 'advResoults.html',{'peptides':pepList, 'searchTerms':searchTerms})
 
+
+# Loads the Advanced Search page
 def advSearch(request):
 	return render(request, 'advSearch.html')
 
+# Collects Data from the advanced search and fech the resoult.
 def advSearchResoults(request):
-	pdb_id = request.GET['pdb_id']
-	hfobic_area = request.GET['hfobic_area']
-	hfobic_avg = request.GET['hfobic_avg']
-	hairpin = request.GET['hairpin']
-	beta_sheet = request.GET['beta_sheet']
-	alpha_helix = request.GET['alpha_helix']
-	alpha_helix_beta_sheet = request.GET['alpha_helix_beta_sheet']
-	alpha_helix_beta_sheet_hairpin = request.GET['alpha_helix_beta_sheet_hairpin']
-	charge = request.GET['charge']
-	m_dipol = request.GET['m_dipol']
-	charge_amt_atm = request.GET['charge_amt_atm']
-	m_dipol_amt_atm = request.GET['m_dipol_amt_atm']
-	sequence = request.GET['sequence']
-	organism = request.GET['organism'] 
-	
-	searchTerms= [{'PDB iD': pdb_id}, {'Hidrofobic Area':hfobic_area}, {'Average Hidrofobicity':hfobic_avg}, {'Hairpin':hairpin}, {'Beta Sheet':beta_sheet}, {'Aplha Helix':alpha_helix}, {'Alpha Helix Beta Sheet':alpha_helix_beta_sheet} , {'Alpha Helix Beta Sheet Hairpin':alpha_helix_beta_sheet_hairpin}, {'Charge':charge}, {'Dipolar Momentum':m_dipol}, {'Charge / Amout of Atoms':charge_amt_atm}, {'Dipolar Momentum / amout of atoms':m_dipol_amt_atm}, {'Sequence':sequence}, {'Organism':organism}]
-	
+	searchTerms=[]
+	#data Fields
+	try:
+		pdb_id = request.GET['pdb_id']
+		searchTerms.append({'PDB iD': pdb_id})
+	except:
+		pass
+
+	try:
+		hfobic_area = request.GET['hfobic_area']
+		searchTerms.append({'Hidrofobic Area':hfobic_area})
+	except:
+		pass
+
+	try:
+		hfobic_avg = request.GET['hfobic_avg']
+		searchTerms.append({'Average Hidrofobicity':hfobic_avg})
+	except:
+		pass
+	try:
+		charge = request.GET['charge']
+		searchTerms.append({'Charge':charge})
+	except:
+		pass
+
+	try:
+		m_dipol = request.GET['m_dipol']
+		searchTerms.append({'Dipolar Momentum':m_dipol})
+	except:
+		pass
+
+	try:
+		charge_amt_atm = request.GET['charge_amt_atm']
+		searchTerms.append({'Charge / Amout of Atoms':charge_amt_atm})
+	except:
+		pass
+
+	try:
+		m_dipol_amt_atm = request.GET['m_dipol_amt_atm']
+		searchTerms.append({'Dipolar Momentum / amout of atoms':m_dipol_amt_atm})
+	except:
+		pass
+
+	try:
+		sequence = request.GET['sequence']
+		searchTerms.append({'Sequence':sequence})
+	except:
+		pass
+
+	try:
+		organism = request.GET['organism']
+		searchTerms.append({'Organism':organism})
+	except:
+		pass
+
+	#Boolean Fields
+	try:
+		hairpin = request.GET['hairpin']
+		if hairpin is 'on':
+			hairpin = True
+	except:
+		hairpin = False
+	try:
+		beta_sheet = request.GET['beta_sheet']
+		if beta_sheet is 'on':
+			beta_sheet = True
+	except:
+		beta_sheet = False
+
+	try:
+		alpha_helix = request.GET['alpha_helix']
+		if alpha_helix is 'on':
+			alpha_helix = True
+	except:
+		alpha_helix = False
+
+	try:
+		alpha_helix_beta_sheet = request.GET['alpha_helix_beta_sheet']
+		if alpha_helix_beta_sheet is 'on':
+			alpha_helix_beta_sheet = True
+	except:
+		alpha_helix_beta_sheet = False
+
+	try:
+		alpha_helix_beta_sheet_hairpin = request.GET['alpha_helix_beta_sheet_hairpin']
+		if alpha_helix_beta_sheet_hairpin in 'on':
+			alpha_helix_beta_sheet_hairpin = True
+	except:
+		alpha_helix_beta_sheet_hairpin = False
+
+	try:
+			searchTerms.append({'Hairpin':hairpin})
+			searchTerms.append({'Beta Sheet':beta_sheet})
+			searchTerms.append({'Aplha Helix':alpha_helix})
+			searchTerms.append({'Alpha Helix Beta Sheet':alpha_helix_beta_sheet})
+			searchTerms.append({'Alpha Helix Beta Sheet Hairpin':alpha_helix_beta_sheet_hairpin})
+	except Exception as e:
+		raise # Failed to append Boolean Fields
+
+
 	return render(request, 'advResoults.html', {'searchTerms':searchTerms})
