7aec51bbbca68f061637e7a55da7186ccf95c90a
rafael <rafa_auyer@icloud.com>
2016-11-25 12:00:07 -0200

    Search terms no longer active when empty

--- ifbAMPdb/ifbAMPdatabase/views.py
+++ ifbAMPdb/ifbAMPdatabase/views.py
@@ -88,100 +88,116 @@ def advSearchResoults(request):
 	#data Fields
 	try:
 		pdb_id = request.GET['pdb_id']
-		searchTerms.append({'PDB iD': pdb_id})
+		if not pdb_id is "":
+			searchTerms.append({'PDB iD': pdb_id})
 	except:
 		pass
 
 	try:
 		hfobic_area = request.GET['hfobic_area']
-		searchTerms.append({'Hidrofobic Area':hfobic_area})
+		if not hfobic_area is "":
+			searchTerms.append({'Hidrofobic Area':hfobic_area})
 	except:
 		pass
 
 	try:
 		hfobic_avg = request.GET['hfobic_avg']
-		searchTerms.append({'Average Hidrofobicity':hfobic_avg})
+		if not hfobic_avg is "":
+			searchTerms.append({'Average Hidrofobicity':hfobic_avg})
 	except:
 		pass
 	try:
 		charge = request.GET['charge']
-		searchTerms.append({'Charge':charge})
+		if not charge is "":
+			searchTerms.append({'Charge':charge})
 	except:
 		pass
 
 	try:
 		m_dipol = request.GET['m_dipol']
-		searchTerms.append({'Dipolar Momentum':m_dipol})
+		if not m_dipol is "":
+			searchTerms.append({'Dipolar Momentum':m_dipol})
 	except:
 		pass
 
 	try:
 		charge_amt_atm = request.GET['charge_amt_atm']
-		searchTerms.append({'Charge / Amout of Atoms':charge_amt_atm})
+		if not charge_amt_atm is "":
+			searchTerms.append({'Charge / Amout of Atoms':charge_amt_atm})
 	except:
 		pass
 
 	try:
 		m_dipol_amt_atm = request.GET['m_dipol_amt_atm']
-		searchTerms.append({'Dipolar Momentum / amout of atoms':m_dipol_amt_atm})
+		if not m_dipol_amt_atm is "":
+
+			searchTerms.append({'Dipolar Momentum / amout of atoms':m_dipol_amt_atm})
 	except:
 		pass
 
 	try:
 		sequence = request.GET['sequence']
-		searchTerms.append({'Sequence':sequence})
+		if not sequence is "":
+			searchTerms.append({'Sequence':sequence})
 	except:
 		pass
 
 	try:
 		organism = request.GET['organism']
-		searchTerms.append({'Organism':organism})
+		if not organism is "":
+			searchTerms.append({'Organism':organism})
 	except:
 		pass
 
 	#Boolean Fields
 	try:
-		hairpin = request.GET['hairpin']
-		if hairpin is 'on':
-			hairpin = True
+		useCheckbox = request.GET['useCheckbox']
 	except:
-		hairpin = False
-	try:
-		beta_sheet = request.GET['beta_sheet']
-		if beta_sheet is 'on':
-			beta_sheet = True
-	except:
-		beta_sheet = False
+		 useCheckbox = False
+	if useCheckbox is 'on':
+		try:
+			hairpin = request.GET['hairpin']
+			if hairpin is 'on':
+				hairpin = True
+		except:
+			hairpin = False
+		try:
+			beta_sheet = request.GET['beta_sheet']
+			if beta_sheet is 'on':
+				beta_sheet = True
+		except:
+			beta_sheet = False
 
-	try:
-		alpha_helix = request.GET['alpha_helix']
-		if alpha_helix is 'on':
-			alpha_helix = True
-	except:
-		alpha_helix = False
+		try:
+			alpha_helix = request.GET['alpha_helix']
+			if alpha_helix is 'on':
+				alpha_helix = True
+		except:
+			alpha_helix = False
 
-	try:
-		alpha_helix_beta_sheet = request.GET['alpha_helix_beta_sheet']
-		if alpha_helix_beta_sheet is 'on':
-			alpha_helix_beta_sheet = True
-	except:
-		alpha_helix_beta_sheet = False
+		try:
+			alpha_helix_beta_sheet = request.GET['alpha_helix_beta_sheet']
+			if alpha_helix_beta_sheet is 'on':
+				alpha_helix_beta_sheet = True
+		except:
+			alpha_helix_beta_sheet = False
 
-	try:
-		alpha_helix_beta_sheet_hairpin = request.GET['alpha_helix_beta_sheet_hairpin']
-		if alpha_helix_beta_sheet_hairpin in 'on':
-			alpha_helix_beta_sheet_hairpin = True
-	except:
-		alpha_helix_beta_sheet_hairpin = False
+		try:
+			alpha_helix_beta_sheet_hairpin = request.GET['alpha_helix_beta_sheet_hairpin']
+			if alpha_helix_beta_sheet_hairpin in 'on':
+				alpha_helix_beta_sheet_hairpin = True
+		except:
+			alpha_helix_beta_sheet_hairpin = False
 
-	try:
+		try:
 			searchTerms.append({'Hairpin':hairpin})
 			searchTerms.append({'Beta Sheet':beta_sheet})
 			searchTerms.append({'Aplha Helix':alpha_helix})
 			searchTerms.append({'Alpha Helix Beta Sheet':alpha_helix_beta_sheet})
 			searchTerms.append({'Alpha Helix Beta Sheet Hairpin':alpha_helix_beta_sheet_hairpin})
-	except Exception as e:
-		raise # Failed to append Boolean Fields
+		except Exception as e:
+			raise # Failed to append Boolean Fields
 
 
 	return render(request, 'advResoults.html', {'searchTerms':searchTerms})
+
