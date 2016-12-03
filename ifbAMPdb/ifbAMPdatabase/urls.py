056b37eacaae13f0713bcf33728d93d56141ee43
Rafael Auyer <rafa_auyer@icloud.com>
2016-12-03 15:31:58 -0200

    add / to search urls

--- ifbAMPdb/ifbAMPdatabase/urls.py
+++ ifbAMPdb/ifbAMPdatabase/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
 	url(r'^$', views.home, name='Home'),
 	url(r'^search/peptide/(?P<pk>[A-Za-z1-9.:]+)/$', views.detail, name='PeptideDetails'),
 	url(r'^search/advanced/$', views.advSearch, name='AdvancedSearch'),
-	url(r'^search/resoults/basic', views.ampBasicSearch, name='SearchResoults'),
-	url(r'^search/resoults/advanced', views.advSearchResoults, name='AdvancedResoults'),
+	url(r'^search/resoults/basic/', views.ampBasicSearch, name='SearchResoults'),
+	url(r'^search/resoults/advanced/', views.advSearchResoults, name='AdvancedResoults'),
 	url(r'', include('blog.urls')),
 	url(r'^admin/', admin.site.urls),
 ]
